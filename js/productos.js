21f88fc591feb4cb024bceea9167e2eed49d1e67
KarlaEstay <karlaestay1990@hotmail.com>
2018-10-15 01:49:02 -0300

    N
    N

--- js/productos.js
+++ js/productos.js
@@ -1,19 +1,71 @@
-var productosNuevos = [];
-
-var accesorios = [];
-
-
-var etc = [{
-  img: "52.png",
-  nombre: "Facebook",
-  etiqueta: "Sin stock",
-  precio: 10000,
+var masVendidos =  [{
+  img: "4.png",
+  nombre: "Libreta Aki",
+  etiqueta: "Nuevo",
+  precio: 2000,
   link: "https://www.kh.com/fnd8fs"
 }, {
-  img: "53.png",
-  nombre: "Instagram",
+  img: "5.png",
+  nombre: "Aros pentagrama corazón",
+  etiqueta: "Nuevo",
+  precio: 3500,
+  link: ""
+}, {
+  img: "3.png",
+  nombre: "Aros",
+  etiqueta: "Nuevo",
+  precio: 3500,
+  link: ""
+}, {
+  img: "1.png",
+  nombre: "Aros Darth Vader",
+  etiqueta: "Nuevo",
+  precio: 4000,
+  link: ""
+}, {
+  img: "2.png",
+  nombre: "Aros Alianza Rebelde",
+  etiqueta: "Nuevo",
+  precio: 4000,
+  link: ""
+}, {
+  img: "6.png",
+  nombre: "Collares Gatitos Yin Yang",
   etiqueta: "",
-  precio: 150000,
+  precio: 4000,
+  link: ""
+}, {
+  img: "7.png",
+  nombre: "Collar Arnold",
+  etiqueta: "",
+  precio: 4000,
+  link: ""
+}];
+
+
+var lineas = [{
+  img: "ouija.png",
+  nombre: "Ouija",
+  etiqueta: "",
+  precio: 2000,
+  link: ""
+}, {
+  img: "aki.png",
+  nombre: "Aki",
+  etiqueta: "",
+  precio: 4000,
+  link: ""
+}, {
+  img: "fanart.png",
+  nombre: "Fanart",
+  etiqueta: "",
+  precio: 4000,
+  link: ""
+}, {
+  img: "kawaii.png",
+  nombre: "Kawaii",
+  etiqueta: "",
+  precio: 4000,
   link: ""
 }];
 
@@ -22,6 +74,10 @@ var etc = [{
 
 
 
-insertItemsInElementId(productosNuevos, 'divNuevos');
-insertItemsInElementId(accesorios, 'divAccesorios');
-insertItemsInElementId(etc, 'divEtc');
+
+
+
+
+
+insertItemsInElementId(masVendidos, 'divMasVendidos');
+insertItemsInElementId(lineas, 'divlineas');
