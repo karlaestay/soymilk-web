18d435ca94c1c8c1dadcd3fc83faeee03460c660
KarlaEstay <karlaestay1990@hotmail.com>
2018-10-15 22:00:50 -0300

    remover cosas extra

--- js/visorimagenescomprar.js
+++
@@ -1,9 +0,0 @@
-function cambiar(evento, timeOut=200,timeIn=200) {
-  var principal = document.getElementById("principal");
-  $("#principal").fadeOut(timeOut,"linear",function(){principal.src=evento.target.src;}).fadeIn(timeIn);
-}
-
-var miniaturas = document.getElementById("miniaturas").children;
-for (var i = 0; i < miniaturas.length; i++) {
-  miniaturas[i].onclick = (evento) => cambiar(evento,200,200);
-}
